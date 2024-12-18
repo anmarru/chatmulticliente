789d8f0b56fd0b123ba2356d45c844bc16b48c12
andrea <andreamartinezruiz10@gmail.com>
2024-12-18 13:46:58 +0100

    clases

--- src/main/java/andrea/servidor/HiloConexion.java
+++ src/main/java/andrea/servidor/HiloConexion.java
@@ -1,8 +1,11 @@
 package andrea.servidor;
 
+import java.net.Socket;
+
 public class HiloConexion implements Runnable{
 
-    
+    private Socket conexion;
+    private 
 
     @Override
     public void run() {
--- src/main/java/andrea/servidor/LanzadorServidor.java
+++ src/main/java/andrea/servidor/LanzadorServidor.java
@@ -2,26 +2,33 @@ package andrea.servidor;
 
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.ArrayList;
 
 public class LanzadorServidor implements Runnable {
 
-    public ServidorChat interfaz;
+    public ServidorChat servidor;
+    ArrayList <HiloConexion> conexiones; 
 
-
+    LanzadorServidor(ServidorChat interfaz){
+        this.servidor= servidor;
+        this.conexiones= conexiones;
+    }
 
     @Override
     public void run() {
         // TODO Auto-generated method stub
         
         try (DatagramSocket serverSoket = new DatagramSocket(9876)) {
+            servidor.escribirMensaje("### Servidor iniciado");
+
+            while () {
+                
+            }
             
         } catch (SocketException e) {
             
             e.printStackTrace();
         }
-        
-        
-        
         
     }
     
