eb257bf5b9ffcc7f205d7815f992f1124103a448
andrea <andreamartinezruiz10@gmail.com>
2024-12-18 13:47:38 +0100

    clases

--- src/main/java/andrea/servidor/ServidorChat.java
+++ src/main/java/andrea/servidor/ServidorChat.java
@@ -1,5 +1,16 @@
 package andrea.servidor;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class ServidorChat {
     
+
+    public ServidorChat(){
+        new Thread(new LanzadorServidor(this)).start();
+    }
+
+    public void escribirMensaje(String mansaje){
+        System.err.println(mansaje);
+    }
 }
